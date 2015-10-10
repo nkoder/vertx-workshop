f83dc3c306bc26f64c124b390f4df66c267b418d
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-10-10 10:10:27 +0200

    Advice about workers

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ run
 
 It's recommended to have one 'starter' verticle which deploys other verticles.
 
+blocking time limit
+-------------------
+If we need to do sth that blocks thread for longer time (like 2 seconds and more)
+then it's preferable to use "workers".
+
 logger
 ------
 
--- src/main/java/io/vertx/workshop/boot/Starter.java
+++ src/main/java/io/vertx/workshop/boot/Starter.java
@@ -1,6 +1,7 @@
 package io.vertx.workshop.boot;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -11,12 +12,12 @@ public class Starter extends AbstractVerticle {
 
     public static void main(String [] args) {
         LOG.info("main");
-        Vertx.vertx().deployVerticle(Starter.class.getName());
+        Vertx.vertx().deployVerticle(Starter.class.getName(), new DeploymentOptions().setInstances(5));
     }
 
     @Override
     public void start() throws Exception {
-        LOG.info("Starting vert.x 3 application");
+        LOG.info("Starting vert.x 3 application in thread " + Thread.currentThread().getName());
 
         LOG.info("Ready!");
     }
